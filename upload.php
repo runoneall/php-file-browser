d784431f2593e4cb8fcc9090cab958c0dfe64be1
runoneall <134772645+runoneall@users.noreply.github.com>
2024-04-05 15:59:07 +0800

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+$current_dir = $_SESSION["current_dir"];
+$host = $_SESSION["HTTP_Host"];
+if (isset($_FILES['file'])) {
+    $file = $_FILES['file'];
+
+    // 检查文件大小
+    if ($file['size'] > 1024*1024*40) {
+        echo "文件太大，请上传小于 40MB 的文件。";
+        exit;
+    }
+
+    // 保存文件
+    $targetFile = (string)$current_dir . "/" . $file['name'];
+    move_uploaded_file($file['tmp_name'], $targetFile);
+
+    echo "文件上传成功！";
+    // echo $current_dir;
+    header("Location: http://".$host."/?dir=".$current_dir);
+} else {
+    echo "请选择要上传的文件。";
+}
+
+?>
